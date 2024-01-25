1dcf92473cff09964aaa41807adcd9c4b07ea522
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2024-01-25 13:17:22 +0600

    feat: add controllers, routes and deps for password reset

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
-import { retrieveAllTweets, retrieveFollowingTweets, retrieveUserTweets } from "../services/index.js"
+import { retrieveAllTweets, retrieveFollowingTweets, retrieveUserTweets, sendPasswordRequest, verifyPasswordReset } from "../services/index.js";
+
 export function getAllTweets(req, res) {
     const tweets = retrieveAllTweets();
     return res.send(tweets);
@@ -8,8 +9,27 @@ export function getUserTweets(req, res) {
     const tweets = retrieveUserTweets(userId);
     return res.send(tweets);
 }
-export function getUserFollowingTweets(req, res) {
+export async function getUserFollowingTweets(req, res) {
     const userId = Number(req.params.userId);
-    const tweets = retrieveFollowingTweets(userId);
+    const tweets = await retrieveFollowingTweets(userId);
     return res.send(tweets);
+}
+
+export function requestPasswordReset(req, res) {
+    if (sendPasswordRequest(req.body.email)) {
+        return res.send({ message: "Password Reset Request sent to your mail" });
+    }
+    return res.sendStatus(500)
+}
+
+export async function resetPassword(req, res) {
+    const token = req.query.token;
+    if(!token) {
+        return res.status(400)
+    }
+    const verification = await verifyPasswordReset(token)
+    if (!verification) {
+        return res.status(400).send("Password cannot be reset")
+    }
+    return res.send("Password can be reset");
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getAllTweets, getUserFollowingTweets, getUserTweets } from "../../controllers/index.js";
+import { getAllTweets, getUserFollowingTweets, getUserTweets, requestPasswordReset, resetPassword } from "../../controllers/index.js";
+import { asyncHandler } from "../../utils/index.js";
+
 const router = Router();
 
 router.get('/tweets', getAllTweets);
-router.get('/users/:userId/tweets/following', getUserFollowingTweets);
+router.get('/users/:userId/tweets/following', asyncHandler(getUserFollowingTweets));
 router.get('/users/:userId/tweets', getUserTweets);
-
+router.get('/forgot-password', requestPasswordReset);
+router.get('/reset-password', asyncHandler(resetPassword));
 export default router;
