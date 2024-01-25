db8dda1930abbee80a6db8db6175fa6f4a3a5a9a
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2024-01-25 13:13:59 +0600

    refactor: remove repository folder

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,57 @@
-import { getAllTweets, getFollowingTweets, getAllUserTweets } from "../repository/index.js"
+import { stringComparator } from '../utils/index.js';
+import queue from "../queue.js";
+import client from "../cache.js";
+import jsonDb from '../../db.json' assert { type: "json" };
+let db;
+
+export function initializeDatabase() {
+    db = jsonDb || {};
+    console.log('Database initialized', db);
+}
+
 
 export function retrieveAllTweets() {
-    return getAllTweets()
+    db.tweets.sort((a, b) => stringComparator(a.timestamp, b.timestamp));
+    return db.tweets;
 }
 
 export function retrieveUserTweets(userId) {
-    return getAllUserTweets(userId);
+    const query = db.tweets.filter(({ author }) => author === userId);
+    return query;
 }
 
-export function retrieveFollowingTweets(userId) {
-    return getFollowingTweets(userId);
+export async function retrieveFollowingTweets(userId) {
+    const user = db.users.find(({ id }) => userId === id);
+    if (!user) {
+        return [];
+    }
+    const cachedQuery = await client.get(String(userId))
+    if (cachedQuery) {
+        return JSON.parse(cachedQuery);
+    }
+    const query = db.tweets.filter(({ author }) => user.following.includes(author))
+    if (query) {
+        await client.set(String(userId), JSON.stringify(query), {
+            EX: 5,
+        })
+        console.log('returning', query)
+        return query;
+    }
+    return [];
+}
+
+export function sendPasswordRequest() {
+    const queueResponse = queue.add('email: ', {
+        body: 'Password reset request received!',
+        email: 'rahman.aryan07@gmail.com' // user.email
+    }, { delay: 2000 })
+    return queueResponse
+}
+
+export async function verifyPasswordReset(token) {
+    const tokenExists = await client.get(`token:${token}`)
+    if (tokenExists) {
+        return true
+    }
+    return false
 }
